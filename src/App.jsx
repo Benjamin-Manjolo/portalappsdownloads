842cd8663c3024be0740fd6a31dcc44c1c676c53
bed-com-02-21 <bed-com-02-21@unima.ac.mw>
2025-04-18 15:00:18 +0200

    images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ImageGallerry from "./ImageGallery";
+import Pansi from "./Pansi";
 export default function App() {
   return (
-    <>
+    <React.Fragment className="flex justify-center">
       <div className="bg-gray-50 rounded-1xl flex flex-col h-auto w-auto p-4">
         <h1 className="font-normal">BENJAMIN MANJOLO</h1>
-        <p className="font-light">
+        <p className="font-light md:text-red-700">
           I am Benjamin Manjolo and i have skills in graphic designing, Web
           development and software engeenering.
           <br />
@@ -15,6 +16,7 @@ export default function App() {
         </p>
       </div>
       <ImageGallerry />
-    </>
+      <Pansi />
+    </React.Fragment>
   );
 }
