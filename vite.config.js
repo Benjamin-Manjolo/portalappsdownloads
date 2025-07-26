6ef18914ddd38af27ad429ce34efe4c36f726b00
bed-com-02-21 <bed-com-02-21@unima.ac.mw>
2025-07-26 18:57:37 +0200

    postcss

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
-  base: "./", // important for Vercel or manual static deployment
+  base: "./",
   server: {
     proxy: {
       "/api": {
